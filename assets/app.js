3860296b39557d1765a10df4e483255e9b4fd52c
Ryan Aldred <shop@Ryans-MacBook-Pro.local>
2021-04-27 16:41:30 -0700

    search for movies function

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,26 @@
-console.log('hello')
+const ajax = (function() {
+    const apiKey = "c23f3411";
+    const baseUrl = `http://www.omdbapi.com/?apikey=${apiKey}&`;
+
+  const publicFunctions = {
+    searchMovies: function(type, query){
+      fetch(`${baseUrl}${type}=${query}`)
+      .then(res => res.json())
+      .then(data => {
+        if(data.Response == "True") {
+          console.log('valid search, do stuff');
+        } else {
+          console.log('invalid search: ' + data.Error);
+        }
+      })
+      .catch(err => console.log('something went wrong: ' + err));
+    }
+  }
+  return publicFunctions;
+})();
+
+
+// example
+// s is for search
+ajax.searchMovies('s', 'fight');
+// ajax.searchMovies('s', 'fightjfkdsjf');
