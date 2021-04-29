a93c0df22351eafefeb0bffea3bb03ffe2c85804
Ryan Aldred <shop@Ryans-MacBook-Pro.local>
2021-04-29 16:22:19 -0700

    render search results on screen - ugly

--- assets/app.js
+++ assets/app.js
@@ -27,22 +27,47 @@ const ajax = (function() {
 // ajax.searchMovies('s', 'fight');
 // ajax.searchMovies('s', 'fightjfkdsjf');
 
-shoppie.Search = function Search(searchInput, resultsList) {
+shoppie.Search = function Search(searchInput, /*resultsList*/) {
   this.selectors = {
     searchInput: document.querySelector(searchInput),
-    resultsList: document.querySelector(resultsList)
+    // resultsList: document.querySelector(resultsList)
   }
 
   // listen for interaction with input and get movies
   // this.selectors.searchInput.addEventListener('input', e => {
-    // this._onSearch(e.target.value);
+  //   this._onSearch(e.target.value);
   // });
-  this.selectors.searchInput.addEventListener('input', utilities._debounce(this._searchMovies, 500));
+  this.selectors.searchInput.addEventListener('input', utilities._debounce(this._onSearch, 500));
 };
 
 shoppie.Search.prototype = Object.assign({}, shoppie.Search.prototype, {
-  _searchMovies: function(e) {
-    ajax.searchMovies("s", e.target.value);
+  _onSearch: function(e) {
+    const apiKey = "c23f3411";
+    const baseUrl = `http://www.omdbapi.com/?apikey=${apiKey}&`;
+    fetch(`${baseUrl}s=${e.target.value}`)
+    .then(res => res.json())
+    .then(data => {
+      if(data.Response == "True") {
+        // search returned movies, now render them
+        const movies = data.Search;
+        const resultsList = document.querySelector('.results__list');
+        console.log(data);
+        for (let movie of movies) {
+          const item = document.createElement('li');
+          item.classList = 'results__list-item';
+          item.innerHTML = `
+            ${movie.Title}
+          `;
+          // console.log(this);
+          resultsList.appendChild(item);
+        }
+      } else {
+        // search didn't return movies, explain why
+        console.log('Invalid search: ' + data.Error);
+      }
+    })
+    // search request failed
+    .catch(err => console.log('Something went wrong: ' + err));
   },
   _renderMovies: function(movies) {
     for (let movie of movies) {
@@ -59,6 +84,8 @@ shoppie.Utilities = function Render() {
 
 shoppie.Utilities.prototype = Object.assign({}, shoppie.Utilities.prototype, {
   _debounce: function(func, delay = 1000) {
+    console.log('debounce this:')
+    console.log(this);
     let timeoutId;
     return function(...args) {
       if(timeoutId) {
@@ -73,4 +100,4 @@ shoppie.Utilities.prototype = Object.assign({}, shoppie.Utilities.prototype, {
 });
 
 const utilities = new shoppie.Utilities();
-const search = new shoppie.Search(".search__input", ".results__list");
+const search = new shoppie.Search(".search__input"/*, ".results__list"*/);
